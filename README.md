9a0d60f836ebc8e7c52d7dbde53f6194cb2adf3e
Olivexicklop <olivex@foxmail.com>
2019-02-27 12:50:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PostApocalypse
---
+++ Source/PostApocalypse/PostApocalypseGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PostApocalypseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POSTAPOCALYPSE_API APostApocalypseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
